ca0401183f9bec84091c373f35cc4a3a8437fff2
OussamaAlouat <al317257@uji.es>
2019-01-25 07:33:23 +0100

    Correct the expected message

--- tests/001-divisors.test.js
+++ tests/001-divisors.test.js
@@ -1,8 +1,8 @@
 import test from 'tape';
 import {getDivisores} from "../index";
 
-test('-------- Divisors', (assert) => {
-    const message = 'Result should be 3';
+test('---- Divisors-----', (assert) => {
+    const message = 'Number 4 have 3 divisors, so the result should be 3';
     const expectedResult = 3;
     getDivisores(4)
         .then((res) => {
